4eda6f482580d7d6b4c828b43111d41a036a4917
esteban <estebangonza2005@gmail.com>
2023-05-04 18:04:59 -0500

    Final

--- InterfacesGraficas/src/Events/MultipleSoruces.java
+++ InterfacesGraficas/src/Events/MultipleSoruces.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class MultipleSoruces {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Instanciamos el objeto de la ventana
 		Marco ventana = new Marco();
 		ventana.setVisible(true);
 	}
@@ -16,9 +16,11 @@ public class MultipleSoruces {
 
 class Marco extends JFrame {
 	public Marco (){
+		//Agregamos lo metodos de la clase JFrame que usará nuestra ventana
 		setTitle("Prueba acciones multiples");
 		setBounds(600, 350, 600, 300);
 		
+		//Instanciamos el objeto que creará la lamina y despues la agregamos
 		PanelAccion lamina = new PanelAccion();
 		add(lamina);
 		
@@ -27,38 +29,47 @@ class Marco extends JFrame {
 
 class PanelAccion extends JPanel {
 	public PanelAccion() {
+		
+		//Instanciamos las acciones de nuestros eventos
 		ActionColor accionAmarillo = new ActionColor("Amarillo", new ImageIcon("src/graficos/image.png"), Color.YELLOW);
 		ActionColor accionAzul = new ActionColor("Azul", new ImageIcon("src/graficos/image.png"), Color.BLUE);
 		ActionColor accionRojo = new ActionColor("Rojo", new ImageIcon("src/graficos/image.png"), Color.RED);
 		
-		//JButton botonAmarillo = new JButton(accionAmarillo);
-		
+		// Creamos los botones con las acciones añadidas
 		add(new JButton(accionAmarillo));
 		add(new JButton(accionAzul));
 		add(new JButton(accionRojo));
 		
-		/*JButton botonAmarillo = new JButton("Amarillo");
 		
-		JButton botonRojo = new JButton("Rojo");
+		//Creamos un Input Map para las teclas
+		InputMap mapaEntrada = getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		
-		JButton botonAzul = new JButton("Azul");
+		mapaEntrada.put(KeyStroke.getKeyStroke("ctrl A"), "fondo_amarillo");
+		mapaEntrada.put(KeyStroke.getKeyStroke("ctrl R"), "fondo_rojo");
+		mapaEntrada.put(KeyStroke.getKeyStroke("ctrl B"), "fondo_azul");
 		
-		add(botonAmarillo);
-		add(botonAzul);
-		add(botonRojo); */
+		ActionMap mapaAccion = getActionMap();
 		
+		// Agregamos las acciones con las keys
+		mapaAccion.put("fondo_amarillo", accionAmarillo);
+		mapaAccion.put("fondo_rojo", accionRojo);
+		mapaAccion.put("fondo_azul", accionAzul);
+		
+	
 		
 	}
 	
+	//Objeto encargado de las acciones del evento
 	class ActionColor extends AbstractAction {
 
+		//Constructor que inserta los datos
 		public ActionColor (String nombre, Icon icono, Color color_boton) {
 			putValue(Action.NAME, nombre);
 			putValue(Action.SMALL_ICON, icono);
 			putValue(Action.SHORT_DESCRIPTION, "Poner la lamina de color " + nombre);
 			putValue("color_de_fondo", color_boton);
 		}
-		
+		// Metodo que se ejecuta al activarse el evento
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			Color c = (Color) getValue("color_de_fondo");
