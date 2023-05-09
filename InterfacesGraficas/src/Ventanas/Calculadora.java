82fbcb33a0b4fe1b798e81604b635b6cf10c0a76
esteban <estebangonza2005@gmail.com>
2023-05-08 19:52:17 -0500

    Final

--- InterfacesGraficas/src/Ventanas/Calculadora.java
+++ InterfacesGraficas/src/Ventanas/Calculadora.java
@@ -1,6 +1,8 @@
 package Ventanas;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -22,48 +24,128 @@ class Marco extends JFrame {
 		
 		LaminaCalculadora miLamina = new LaminaCalculadora();
 		add(miLamina);
+		
+		//pack(); // La ventana toma el tamaño default de lo que hay en su interior
 	}
 }
 
 class LaminaCalculadora extends JPanel {
 	private JPanel body;
+	private JButton pantalla;
+	private boolean principio;
+	
 	public LaminaCalculadora () {
+		this.principio = true;
+		
+		// Configuramos el tipo de layout para configurar el display de la calculadora
 		setLayout(new BorderLayout());
 		
-		JButton pantalla = new JButton("0");
+		pantalla = new JButton("0");
 		pantalla.setEnabled(false);
 		
+		//Añadimos el display y lo situamos en el lado norte
 		add(pantalla, BorderLayout.NORTH);
 		
+		// Creamos la lamina donde situaremos el teclado de la calculadora
 		this.body = new JPanel();
-		body.setLayout(new GridLayout(4,4));
+		body.setLayout(new GridLayout(4,4)); // Hacemos el layout en forma de grid
 		
-		ponerBoton("7");
-		ponerBoton("8");
-		ponerBoton("9");
-		ponerBoton("/");
+		ActionListener insertarOrden = new InsertaNumero(); // Evento de insertar el numero
+		ActionListener accionOrden = new AccionOrden(); // Evento de accionar la orden
 		
-		ponerBoton("4");
-		ponerBoton("5");
-		ponerBoton("6");
-		ponerBoton("x");
+		//Agregamos los botones junto con su respectiva accion
+		ponerBoton("7", insertarOrden);
+		ponerBoton("8", insertarOrden);
+		ponerBoton("9", insertarOrden);
+		ponerBoton("/", accionOrden);
 		
-		ponerBoton("1");
-		ponerBoton("2");
-		ponerBoton("3");
-		ponerBoton("-");
+		ponerBoton("4", insertarOrden);
+		ponerBoton("5", insertarOrden);
+		ponerBoton("6", insertarOrden);
+		ponerBoton("x", accionOrden);
 		
-		ponerBoton("0");
-		ponerBoton(".");
-		ponerBoton("+");
-		ponerBoton("=");
+		ponerBoton("1", insertarOrden);
+		ponerBoton("2", insertarOrden);
+		ponerBoton("3", insertarOrden);
+		ponerBoton("-", accionOrden);
 		
+		ponerBoton("0", insertarOrden);
+		ponerBoton(".", insertarOrden);
+		ponerBoton("+", accionOrden);
+		ponerBoton("=", accionOrden);
 		
+		//Agregamos la lamina
 		add(body);
 	}
 	
-	private void ponerBoton (String title) {
+	private void ponerBoton (String title, ActionListener event) {
 		JButton boton = new JButton(title);
 		body.add(boton);
+		boton.addActionListener(event);
 	}
+	
+	private class InsertaNumero implements ActionListener {
+
+		@Override //Funcion que desencadena el evento de Insertar Numero
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			String entrada = e.getActionCommand();
+			if (principio) {
+				pantalla.setText("");
+				principio=false;
+			}
+			pantalla.setText(pantalla.getText() + entrada);
+		}
+		
+	}
+	
+	private class AccionOrden implements ActionListener {
+
+		private double cache1;
+		private String accion;
+		private double resultado;
+		
+		@Override //Funcion que desencadena el evento de accionar la operacion
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			String entrada = e.getActionCommand();
+			principio = true;
+			
+			
+			if (entrada == "=") { // Si el boton que se oprimió es el igual:
+				
+				double cache2 = Double.parseDouble(pantalla.getText());
+				
+				pantalla.setText(cache1 + " " + (accion) + " " + cache2 + " = " + calcular(cache2));
+				
+				cache1 = 0;
+			}else { // Guardar la ultima accion
+				accion = entrada;
+				cache1 = Double.parseDouble(pantalla.getText());
+			}
+			
+		}
+		
+		private double calcular (double x) {
+			switch (accion) { // Switch de acciones
+			case "+": 
+				resultado = cache1 + x;
+				break;
+			case "-": 
+				resultado = cache1 - x;
+				break;
+			case "x": 
+				resultado = cache1 * x;
+				break;
+			case "/": 
+				resultado = cache1 / x;
+				break;
+			default: break;
+			}
+			return resultado;
+				
+		}
+		
+	}
+	
 }
