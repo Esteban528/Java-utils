935b24f7c9aac447a477f509d23ae3c2f951246f
esteban <estebangonza2005@gmail.com>
2023-07-10 15:48:15 -0500

    Threads: syncronized Object class

--- trheads/src/trheads/BancoSinSincronizar.java
+++ trheads/src/trheads/BancoSinSincronizar.java
@@ -24,34 +24,31 @@ class Banco {
 			cuentas[i] = 2000;
 		}
 		
-		saldoSuficiente=cierreBanco.newCondition();
 	}
+	// Palabra syncronized permite sincronizar el metodo a los hilos
 	
-	public void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
+	// Este solo se puede usar con una condición
+	public synchronized void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
 		
-		cierreBanco.lock();
+		//cierreBanco.lock();
 		
-		try {
-		
-			while (cuentas [cuentaOrigen]<cantidad) { // Verificar que el saldo no sea inferior a la cantidad
-				saldoSuficiente.await(); // El hilo se duerme meintras la condicion se cumple
-			}
-			
-			System.out.println(Thread.currentThread());
-			
-			cuentas[cuentaOrigen] -= cantidad; // Restar el saldo de la cuenta origen
-			
-			System.out.printf("%19.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
-			
-			cuentas[cuentaDestino] += cantidad; //Sumar el saldo a la cuenta destino
-			
-			System.out.printf("Saldo total:  %10.2f%n", getSaldoTotal());
-			
-			saldoSuficiente.signalAll(); // Esto activa los hilos que estaban a la espera
-			
-		} finally {
-			cierreBanco.unlock();
+		while (cuentas [cuentaOrigen]<cantidad) { // Verificar que el saldo no sea inferior a la cantidad
+			//saldoSuficiente.await(); 
+			wait();	// El hilo se duerme meintras la condicion se cumple
 		}
+			
+		System.out.println(Thread.currentThread());
+		
+		cuentas[cuentaOrigen] -= cantidad; // Restar el saldo de la cuenta origen
+		
+		System.out.printf("%19.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
+		
+		cuentas[cuentaDestino] += cantidad; //Sumar el saldo a la cuenta destino
+		
+		System.out.printf("Saldo total:  %10.2f%n", getSaldoTotal());
+		
+		notifyAll(); // Notifica a los demás hilos que pueden arrancar
+		
 	}
 	
 	public double getSaldoTotal () {
@@ -67,8 +64,6 @@ class Banco {
 	private final double[] cuentas;
 	
 	private ReentrantLock cierreBanco = new ReentrantLock();
-	
-	private Condition saldoSuficiente;
 }
 
 
